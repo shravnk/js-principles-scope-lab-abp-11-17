dbca33ec86fb9ea50304b4812099bb7d6bc3d1b6
Brendan O'Dea <brendan.ode4@gmail.com>
2017-11-11 00:27:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = 'tom'
+function  changeLeastFavoriteCustomer() {
+leastFavoriteCustomer = 'petyr'
+}
+
+
+
+function attemptTwoFavoriteCustomers() {
+  const favoriteCustomer = 'sally'
+  let favoriteCustomer = "me"
+}
